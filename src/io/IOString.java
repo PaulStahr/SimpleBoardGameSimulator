2c2eea35e56edd2f1562211c6e28f7c7efdb2682
Paul Stahr <paul.stahr@gmx.de>
2020-04-09 19:05:24 +0200

    Fixed transmition of owner id, outsourced Strings of GameIO

---
+++ src/io/IOString.java
@@ -0,0 +1,37 @@
+package io;
+
+public class IOString {
+	public static final String X = "x";
+	public static final String Y = "y";
+	public static final String Z = "z";
+	public static final String R = "r";
+	public static final String OWNER_ID = "owner_id";
+	public static final String PLAYER = "player";
+	public static final String ABOVE = "above";
+	public static final String BELOW = "below";
+	public static final String VALUE = "value";
+	public static final String SIDE = "side";
+	public static final String NAME = "name";
+	public static final String HIDDEN = "hidden";
+	public static final String OBJECT = "object";
+	public static final String PASSWORD = "password";
+	public static final String UNIQUE_NAME = "unique_name";
+	public static final String ID = "id";
+	public static final String COLOR = "color";
+	public static final String MOUSE_X = "mouse_x";
+	public static final String MOUSE_Y = "mouse_y";
+	public static final String TYPE = "type";
+	public static final String WIDTH = "width";
+	public static final String HEIGHT = "height";
+	public static final String FRONT = "front";
+	public static final String BACK = "back";
+	public static final String STANDING = "standing";
+	public static final String BACKGROUND = "background";
+	public static final String GAME_XML = "game.xml";
+	public static final String GAME_INSTANCE_XML = "game_instance.xml";
+	public static final String OBJECT_STATE = "object_state";
+	public static final Object PLAYER_XML = "player.xml";
+	public static final String FIGURE = "figure";
+	public static final String DICE = "dice";
+	public static final String CARD = "card";
+}
