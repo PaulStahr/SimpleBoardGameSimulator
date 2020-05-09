0686b6d315ce9e8206479376ae74e422dc69ba78
Paul Stahr <paul.stahr@gmx.de>
2020-05-10 00:21:36 +0200

    Added reset State-Button to Game-Edit-Panel

--- src/gui/EditGameWindow.java
+++ src/gui/EditGameWindow.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 import data.JFrameLookAndFeelUtil;
 import gameObjects.instance.GameInstance;
+import main.Player;
 
 public class EditGameWindow extends JFrame{
 	/**
@@ -11,9 +12,9 @@ public class EditGameWindow extends JFrame{
 	 */
 	private static final long serialVersionUID = -9190386402174391670L;
 	GameInstance gi;
-	public EditGameWindow(GameInstance gi, LanguageHandler lh) {
+	public EditGameWindow(GameInstance gi, LanguageHandler lh, Player player) {
 		this.gi = gi;
-		setContentPane(new EditGamePanel(gi, lh));
+		setContentPane(new EditGamePanel(gi, lh, player));
 		JFrameLookAndFeelUtil.addToUpdateTree(this);
 		setSize(500, 400);
 	}
