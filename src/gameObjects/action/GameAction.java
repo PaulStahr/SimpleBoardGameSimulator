d2a798d50296252b95b9191d145ae4b682a55b75
Paul Stahr <paul.stahr@gmx.de>
2020-05-17 08:56:33 +0200

    Added timing-information, when an Update occured

--- src/gameObjects/action/GameAction.java
+++ src/gameObjects/action/GameAction.java
@@ -8,6 +8,7 @@ public abstract class GameAction  implements Serializable{
 	 */
 	private static final long serialVersionUID = -4972550031815213503L;
 	public final int source;
+	public long when = System.nanoTime();
 	public GameAction(int source)
 	{
 		this.source = source;
