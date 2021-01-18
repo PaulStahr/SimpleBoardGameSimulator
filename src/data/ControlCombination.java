86de8b284f91b8a84c999d50a5ae8ab6ac9a1df8
Paul Stahr <paul.stahr@gmx.de>
2021-01-18 18:46:28 +0100

    Code cleanup

--- src/data/ControlCombination.java
+++ src/data/ControlCombination.java
@@ -10,7 +10,6 @@ public class ControlCombination
 {
 	final int keyModifier;
 	final int mouse;
-	//final char key;
 	final int additional;
 	final int keyCode;
 	
@@ -31,28 +30,15 @@ public class ControlCombination
 		return strB;
 	}
 	
-	public String toString(Language lang)
-	{
-		return toString(new StringBuilder(), lang).toString();
-	}
+	public String toString(Language lang){return toString(new StringBuilder(), lang).toString();}
 
 	public StringBuilder toString(StringBuilder strB, Language lang) {
-		if ((keyModifier & InputEvent.SHIFT_DOWN_MASK) != 0)
-		{
-			appendPlus(strB).append(lang.getString(Words.shift));
-		}
-		if ((keyModifier & InputEvent.CTRL_DOWN_MASK) != 0)
-		{
-			appendPlus(strB).append(lang.getString(Words.ctrl));
-		}
-		if ((keyModifier & InputEvent.ALT_DOWN_MASK) != 0)
-		{
-			appendPlus(strB).append(lang.getString(Words.alt));
-		}
+		if ((keyModifier & InputEvent.SHIFT_DOWN_MASK) != 0)	{appendPlus(strB).append(lang.getString(Words.shift));}
+		if ((keyModifier & InputEvent.CTRL_DOWN_MASK) != 0)		{appendPlus(strB).append(lang.getString(Words.ctrl));}
+		if ((keyModifier & InputEvent.ALT_DOWN_MASK) != 0)		{appendPlus(strB).append(lang.getString(Words.alt));}
 
-		if (keyCode != -1){
-			appendPlus(strB).append(KeyEvent.getKeyText(keyCode));
-		}
+		if (keyCode != -1){appendPlus(strB).append(KeyEvent.getKeyText(keyCode));}
+
 		if (mouse != -1)
 		{
 			switch(mouse)
@@ -63,27 +49,12 @@ public class ControlCombination
 				default: throw new IllegalArgumentException("MouseFunction not known");
 			}
 		}
-		if ((additional & 1) != 0)
-		{
-			appendPlus(strB).append(lang.getString(Words.drag));
-		}
-		if ((additional & 2) != 0)
-		{
-			appendPlus(strB).append(lang.getString(Words.grab));
-		}
-		if ((additional & 4) != 0)
-		{
-			appendPlus(strB).append(lang.getString(Words.rotate_wheel));			
-		}
-		if ((additional & 8) != 0) {
-			appendPlus(strB).append(lang.getString(Words.drop_on_board));
-		}
-		if ((additional & 16) != 0) {
-			appendPlus(strB).append(lang.getString(Words.arrow_keys));
-		}
-		if ((additional & 32) != 0) {
-			appendPlus(strB).append(lang.getString(Words.double_click));
-		}
+		if ((additional & 1) != 0)	{appendPlus(strB).append(lang.getString(Words.drag));}
+		if ((additional & 2) != 0)	{appendPlus(strB).append(lang.getString(Words.grab));}
+		if ((additional & 4) != 0)	{appendPlus(strB).append(lang.getString(Words.rotate_wheel));}
+		if ((additional & 8) != 0)  {appendPlus(strB).append(lang.getString(Words.drop_on_board));}
+		if ((additional & 16) != 0) {appendPlus(strB).append(lang.getString(Words.arrow_keys));}
+		if ((additional & 32) != 0) {appendPlus(strB).append(lang.getString(Words.double_click));}
 
 		return strB;
 	}
