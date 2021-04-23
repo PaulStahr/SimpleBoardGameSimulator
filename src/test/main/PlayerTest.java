80ffddec2bf170822974b61b5aa3ff027c6e1903
FlorianSeiffarth <florian.seiffarth@t-online.de>
2021-04-23 13:47:03 +0200

    PlayerAddTest

--- src/test/main/PlayerTest.java
+++ src/test/main/PlayerTest.java
@@ -9,12 +9,21 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class PlayerTest {
     int id = (int)System.nanoTime();
     @Test
-    public void PlayerColorTest() throws IOException, JDOMException {
+    public void AddPlayerTest() throws IOException, JDOMException {
         GameInstance gi = new GameInstance(new Game(), "Foo");
-        Player pl = new Player("Max", 4);
+        Player pl = new Player("Max1", 4);
+
         gi.addPlayer(new PlayerAddAction(id, pl), pl);
+        int playerNum = gi.getPlayerList().size();
+        Player pl1 = gi.getPlayerById(pl.id);
+        assertFalse(pl1 == null);
+        assertEquals(pl.id, pl1.id);
+        assertEquals(1, playerNum);
     }
 }
